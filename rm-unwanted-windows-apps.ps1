b016ef64662c1893ee9e947fb136c266f094616a
Patrick x0xr00t Hoogeveen <25748471+x0xr00t@users.noreply.github.com>
2021-08-08 23:21:37 +0200

    Add files via upload

---
+++ rm-unwanted-windows-apps.ps1
@@ -0,0 +1,17 @@
+# remove bing weather
+Get-AppxPackage Microsoft.BingWeather | Remove-AppxPackage
+
+# remove microsoft xboxapp
+Get-AppxPackage Microsoft.XboxApp | Remove-AppxPackage
+
+# remove microsoft people
+Get-AppxPackage Microsoft.People | Remove-AppxPackage
+
+# remove windows sound recorder
+Get-AppxPackage Microsoft.WindowsSoundRecorder | Remove-AppxPackage
+
+# remove yourphone 
+Get-AppxPackage Microsoft.YourPhone | Remove-AppxPackage
+
+# remove windowsmaps
+Get-AppxPackage Microsoft.WindowsMaps | Remove-AppxPackage
